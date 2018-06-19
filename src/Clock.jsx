e91dffe309b8a692b9c4d1038ab3791767840fb7
Josue <jmancilla@shutterfly.com>
2018-06-19 01:05:28 -0400

    mejorado la inicializacion de fechas
    ahora se puede establecer fecha inicial

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,31 +1,47 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-
-class Clock extends React.Component{
-  
-  static defaultProps={
-    date:new Date()
-  }
-  
-  state={
-    date:new Date()
+class Clock extends React.Component {
+  static defaultProps = {
+    date: new Date()
   };
-  
-  constructor(){
-    super();
-    setInterval(()=>{
-     this.setState({date:new Date()}) 
-    },1000)
+  /*
+  state = {
+    date: this.
+  };
+*/
+  constructor(props) {
+    super(props);
+    let dateIni = this.props.horaIni ? this.props.horaIni : Date.now();
+    this.state = {
+      date: new Date(dateIni)
+    };
   }
 
-  render(){
+  incrementarFecha() {
+    let currentDate = this.state.date;
+    currentDate.setSeconds(currentDate.getSeconds() + 1);
+    this.setState({ date: currentDate });
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => {
+      this.incrementarFecha();
+    }, 1000);
+    console.log('se monto el componente en el dom');
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    console.log('componente desmontado');
+  }
+
+  render() {
     return (
-    <div>
-      <h1>{this.props.title}</h1>
-      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+      <div>
+        <h1>{this.props.title}</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Clock from './Clock'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Clock from './Clock';
 
-import "./styles.css";
-
-
+import './styles.css';
 
 function tick() {
-  ReactDOM.render(<Clock title="Reloj mejorado sin lifecycles" />, document.getElementById('root'));
+  ReactDOM.render(
+    <section>
+      <Clock
+        title="Reloj mejorado sin lifecycles"
+        horaIni="July 21, 1983 01:15:00"
+      />
+      <Clock title="otro relojito" horaIni="August 02, 2005 03:18:46" />
+      <Clock title="y uno mas" />
+    </section>,
+    document.getElementById('root')
+  );
 }
 tick();
 //setInterval(tick, 1000);
